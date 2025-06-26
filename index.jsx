a3ef7548ab9439239d8a5bfaa24a930170081d3c
almadani02 <milaannik@gmail.com>
2025-06-26 22:58:35 +0700

    Delete index.jsx

--- index.jsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', marginTop: '50px' }}>
-      <h1>Selamat Datang di Ruang Digital</h1>
-      <p>Produk digital siap pakai, aman & praktis</p>
-    </div>
-  );
-}
